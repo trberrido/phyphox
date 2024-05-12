a47966597c0037e7b14864b994ed8f1575f2b154
Thibault Berrido <thibault.berrido@gmail.com>
2024-05-12 21:57:56 +0000

    update front

--- static/js/933.b18f9c4e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkphyphox_front=self.webpackChunkphyphox_front||[]).push([[933],{416:(e,n,t)=>{t.r(n),t.d(n,{default:()=>o});var r=t(43),s=t(141),i=t(175),p=t(579);const o=()=>{const e=(0,r.useContext)(s.cr).isAppListening;return(0,p.jsx)(p.Fragment,{children:(0,p.jsx)(i.A,{title:e?"Ongoing experience, data collection in progress":"Last experiment",isOnGoingExperiment:e,fetchURL:window.API+"/api/experiments/"+(e?"current":"last")})})}}}]);
-//# sourceMappingURL=933.b18f9c4e.chunk.js.map
